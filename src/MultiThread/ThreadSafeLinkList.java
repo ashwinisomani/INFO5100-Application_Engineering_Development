078ef2ff72b13e30a54283dd7b0f1bf6688b1e50
ashwinisomani <ashwini.somanisap5@gmail.com>
2021-11-12 15:00:57 -0800

    Assignemnt Fourth

---
+++ src/MultiThread/ThreadSafeLinkList.java
@@ -0,0 +1,68 @@
+package MultiThread;
+import java.util.LinkedList;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class ThreadSafeLinkList<T> {
+    private LinkedList<T> linkedList = new LinkedList<>();
+
+    ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+
+    public void addAtPosition(int index, T data) {
+        lock.writeLock().lock();
+        linkedList.add(index, data);
+        lock.writeLock().unlock();
+    }
+    public void removeAtPosition(int index ){
+        lock.writeLock().lock();
+        linkedList.remove(index);
+        lock.writeLock().unlock();
+    }
+
+    public T getFirst() {
+        T data;
+        lock.readLock().lock();
+        data = linkedList.getFirst();
+        lock.readLock().unlock();
+        return data;
+    }
+
+    public T getLast() {
+        T data;
+        lock.readLock().lock();
+        data = linkedList.getLast();
+        lock.readLock().unlock();
+        return data;
+    }
+
+    public int size() {
+        int size;
+        lock.readLock().lock();
+        size = linkedList.size();
+        lock.readLock().unlock();
+        return size;
+
+    }
+    public void add(T data) {
+        lock.writeLock().lock();
+        linkedList.add(data);
+        lock.writeLock().unlock();
+    }
+
+    static class Worker<T> implements Runnable {
+
+        ThreadSafeLinkList<T> list;
+        T value;
+
+        public Worker(ThreadSafeLinkList<T> list, T data) {
+            this.list = list;
+            this.value = data;
+        }
+
+        @Override
+        public void run() {
+            for (int index = 0; index < 1000; index++) {
+                list.add(value);
+            }
+        }
+    }
+}
---
+++ src/MultiThread/TrafficLight.java
@@ -0,0 +1,26 @@
+package MultiThread;
+
+class TrafficLight {
+    private boolean isAGreen = true; // set to true if A is greeen.
+
+    public TrafficLight() {
+    }
+
+    public void carArrived(
+            int carId,           // ID of the car
+            int roadId,          // ID of the road the car travels on. Can be 1 (road A) or 2 (road B)
+            int direction,       // Direction of the car
+            Runnable turnGreen,  // Use turnGreen.run() to turn light to green on current road
+            Runnable crossCar    // Use crossCar.run() to make car cross the intersection
+    ) {
+        synchronized(this) {
+            if ((roadId == 1 && !isAGreen) ||
+                    (roadId == 2 && isAGreen)) {
+                // The lights are green for the other road. Flip the light color.
+                isAGreen = !isAGreen;
+                turnGreen.run();
+            }
+            crossCar.run();
+        }
+    }
+}
